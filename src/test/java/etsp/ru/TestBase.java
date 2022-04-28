16f96402e6b6176dce30e25945e945a9e0100cbc
Denis Denisov <79483675+denisofflive@users.noreply.github.com>
2022-04-28 12:46:27 +0300

    Update TestBase.java

--- src/test/java/etsp/ru/TestBase.java
+++ src/test/java/etsp/ru/TestBase.java
@@ -13,7 +13,7 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://etsp.ru";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
